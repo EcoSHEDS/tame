84b74c2e6e857556b0e9e468c901b9554921a61c
Jeff Walker <walker.jeff.d@gmail.com>
2019-11-20 22:22:07 -0500

    update deerfield dataset

--- r/deerfied.R
+++ r/deerfied.R
@@ -6,7 +6,7 @@ library(lubridate)
 
 # load --------------------------------------------------------------------
 
-df_orig <- read_csv("~/Dropbox/SHEDS/tame/data/20191021/telemetryDataFinal telemetryDataFinal_2019-10-18 16_24_45_.csv", col_types = cols(
+df_orig <- read_csv("~/Dropbox/SHEDS/tame/data/20191120/telemetryDataFinal telemetryDataFinal_2019-11-20 15_36_00_.csv", col_types = cols(
   X1 = col_double(),
   taggingSite = col_character(),
   tagID = col_double(),
@@ -38,9 +38,9 @@ df <- df_orig %>%
   ) %>% 
   arrange(datetime, uid) %>% 
   mutate(
-    datetime = mdy_hm(datetime, tz = "US/Eastern")
+    datetime = ymd_hms(datetime, tz = "US/Eastern")
   )
-  
+summary(df)
 
 # domains -----------------------------------------------------------------
 
